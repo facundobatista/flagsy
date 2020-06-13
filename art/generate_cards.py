786b4734de9e7a9ae1cf29566725b20f0d34c898
Facundo Batista <facundo@taniquetil.com.ar>
2020-06-13 07:25:26 -0300

    All code done.

--- art/generate_cards.py
+++ art/generate_cards.py
@@ -2,26 +2,31 @@
 
 import os
 import json
+import operator
 import unicodedata
 
-import certg  # fades file:///home/facundo/devel/reps/certg
+import certg  # fades >=5
+
+RESULT_DIR = 'result'
+
+PROCESSED_OK = 'ok'
+PROCESSED_FLAG = "__processed__"
 
 
 def cback(data):
     """Show progress."""
-    print("  {} {}".format(data['progress'], data['simple_name']))
+    print(" ", data['progress'])
 
 
 def generate_fronts(db):
     """Generate the fronts with just the flags."""
-    item = db[36]
-
-    replace_info = [{
-        'wflag_path': os.path.abspath("pngs/{}.flag.png".format(item['name'])),
-        'progress': item['progress'],
-        'simple_name': item['name'],
-        'reduced_name': item['reduced_name'],
-    }]
+    replace_info = []
+    for item in db:
+        replace_info.append({
+            'wflag_path': item['wflag_path'],
+            'progress': item['progress'],
+            'reduced_name': item['reduced_name'],
+        })
 
     image_info = [{
         'placement_rectangle_id': 'rect19351',
@@ -30,27 +35,44 @@ def generate_fronts(db):
     }]
 
     certg.process(
-        "card-front.svg", "card-front", "reduced_name",
+        "card-front.svg", os.path.join(RESULT_DIR, "card-front"), "reduced_name",
         replace_info, image_info, progress_cb=cback)
+
+
+def is_single(text):
+    """Determine if there are many items in the text."""
+    indicators = [', ', ' y ', ' e ']
+    return not any(ind in text for ind in indicators)
 
 
 def generate_backs(db):
     """Generate the backs with all the rest of the information."""
-    item = db[36]
+    replace_info = []
 
-    replace_info = [{
-        'continent': item['continent'],
-        'capital': item['capital_name'],
-        'languages': item['languages'],
-        'demonyms': item['demonyms'],
-        'codes': item['code'],
-        'original_name': item['name_original'],
-        'translated_name': item['name_translated'],
-        'simple_name': item['name'],
-        'wloc_path': os.path.abspath("pngs/{}.location.png".format(item['name'])),
-        'progress': item['progress'],
-        'reduced_name': item['reduced_name'],
-    }]
+    for item in db:
+        # no point on having both if they are the same
+        original_name = item['name_original']
+        if original_name == item['name_translated']:
+            original_name = ''
+
+        lang_title = "Idioma" if is_single(item['languages']) else "Idiomas"
+        demonym_title = "Gentilicio" if is_single(item['demonyms']) else "Gentilicios"
+
+        replace_info.append({
+            'continent': item['continent'],
+            'capital': item['capital_name'],
+            'lang_title': lang_title,
+            'lang_content': item['languages'],
+            'demonym_title': demonym_title,
+            'demonym_content': item['demonyms'],
+            'codes': item['code'],
+            'original_name': original_name,
+            'translated_name': item['name_translated'],
+            'simple_name': item['name'],
+            'wloc_path': item['wloc_path'],
+            'progress': item['progress'],
+            'reduced_name': item['reduced_name'],
+        })
 
     image_info = [{
         'placement_rectangle_id': 'rect19351',
@@ -59,25 +81,46 @@ def generate_backs(db):
     }]
 
     certg.process(
-        "card-back.svg", "card-back", "reduced_name", replace_info, image_info, progress_cb=cback)
+        "card-back.svg", os.path.join(RESULT_DIR, "card-back"), "reduced_name",
+        replace_info, image_info, progress_cb=cback)
 
 
 def load(dbpath):
     """Load the DB and pre fill it with more info."""
-    with open(dbpath, "rt", encoding="ascii") as fh:
+    with open(dbpath, "rt", encoding="utf8") as fh:
         db = json.load(fh)
 
-    for idx, item in enumerate(db, 1):
-        item['progress'] = "{} ({}/{})".format(item['name'], idx, len(db))
+    db = [item for item in db if item.get(PROCESSED_FLAG) == PROCESSED_OK]
+
+    for item in db:
         name = item['name'].replace('/', '').replace(' ', '')
         item['reduced_name'] = (
             unicodedata.normalize('NFKD', name).encode('ASCII', 'ignore').decode("ASCII").lower())
+
+        name = item['name'].split('/')[0].strip()
+        wloc_path = os.path.abspath("pngs/{}.location.png".format(name))
+        wflag_path = os.path.abspath("pngs/{}.flag.png".format(name))
+        if not os.path.exists(wloc_path) or not os.path.exists(wflag_path):
+            print("exist? {} {!r}".format(os.path.exists(wloc_path), wloc_path))
+            print("exist? {} {!r}".format(os.path.exists(wflag_path), wflag_path))
+            raise ValueError("Missing images for item {}".format(item))
+        item['wloc_path'] = wloc_path
+        item['wflag_path'] = wflag_path
+
+    # order and set the progress indicator
+    db.sort(key=operator.itemgetter('reduced_name'))
+    for idx, item in enumerate(db, 1):
+        item['progress'] = "{} ({}) - {}/{}".format(
+            item['name'], item['reduced_name'], idx, len(db))
 
     return db
 
 
 def main(dbpath):
     """Main entry point."""
+    if not os.path.exists(RESULT_DIR):
+        os.mkdir(RESULT_DIR)
+
     db = load(dbpath)
     generate_fronts(db)
     generate_backs(db)
